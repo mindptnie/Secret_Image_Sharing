cf57496897af5951c4dee7126c7d277f47b17fa2
OwlVI <mrowlvi@gmail.com>
2025-12-03 23:46:02 +0800

    Add model vae

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,9 @@
 from . import vq_vae
 from . import vq_vae_resnet
+from . import vae
 
 vae_models = {
     "vq_vae": vq_vae.VQ_VAE,
-    "vq_vae_resnet": vq_vae_resnet.VQ_VAE_ResNet
+    "vq_vae_resnet": vq_vae_resnet.VQ_VAE_ResNet,
+    "vae": vae.VAE
 }
